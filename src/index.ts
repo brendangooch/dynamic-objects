1510e6ac3f4e6434ca219de5e6b7f1e2801ae670
brendan <brendangooch@gmail.com>
2024-06-01 12:25:30 +0100

    added all project files (excluding tests); configured project. ready to write code and tests

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * barrel file for package
+ */
+
+export { DynamicUnit } from './dynamic-unit.js';
+export { DynamicNumber } from './dynamic-number.js';
+export { DynamicPosition } from './dynamic-position.js';
