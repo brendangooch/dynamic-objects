614284a4a4dbb26457e94b978eced4c886f5af3f
brendan <brendangooch@gmail.com>
2025-06-07 22:08:17 +0100

    cleared out all the old classes ready to copy in the new versions from test project.  Set to v6.0.0, imported all lib  dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,45 @@
  */
 
 import type { tEaseOption } from "@brendangooch/ease";
-import type { iUpdateable } from "@brendangooch/simple-game-loop";
+import type { iUpdateable } from "@brendangooch/game-engine";
+
+export interface iDeferrable {
+    next(): void;
+}
 
 export interface iDynamic extends iUpdateable {
     get isActive(): boolean;
     get isComplete(): boolean;
-    get isRunning(): boolean;
-    get isOn(): boolean;
-    turnOn(): void;
-    turnOff(): void;
-    load(json: string): boolean;
-    save(): string;
-    start(): void;
-    stop(): void;
-    pause(): void;
-    getDuration(): number;
-    duration(ms: number): iDynamic;
-    ease(ease: tEaseOption): iDynamic;
-    onComplete(fn: Function): iDynamic;
-    change(): boolean;
-    tick(): void;
-    step(dir: 1 | -1): void;
-    rewind(): void;
-    endNow(): void;
-    runRate(speed: number): void;
+}
+
+export type tChangeTo = {
+    duration: number;
+    ease: tEaseOption;
 };
 
-export { DynamicUnit } from "./unit/dynamic-unit.js";
+export type tChangeNumberTo = tChangeTo & {
+    value: number;
+};
+
+export type tChangePositionTo = tChangeTo & {
+    x: number,
+    y: number,
+    angle?: number,
+    distance?: number
+};
+
+export type tChangeRotationTo = tChangeNumberTo & {
+    spin: number;
+}
+
+export type tChangeStringTo = tChangeTo & {
+    value: string;
+}
+
+export type tChangeWaveTo = tChangeTo & {
+    numCycles: number;
+    up: boolean;
+    square: boolean;
+};
+
+//
